e0c3d27fe4497b773fde2751ec27ce9472ca6cfb
Julia <63446729+mharriso@users.noreply.github.com>
2021-01-13 19:23:44 +0300

    update ft_printf.c

--- ft_printf.c
+++ ft_printf.c
@@ -16,13 +16,15 @@ static	int		parse_and_print(char **format, va_list args, t_flags *flags)
 {
 	int		res;
 
+	if (**format == '\0')
+		return (0);
 	ft_parser(format, args, flags);
 	if ((res = print_format_arg(**format, args, flags)) == -1)
 	{
 		va_end(args);
-		return (res);
+		return (-1);
 	}
-	if (res)
+	if (res == 1)
 		free(flags->str);
 	if (**format)
 		(*format)++;
@@ -43,9 +45,8 @@ int				ft_printf(const char *format, ...)
 		{
 			flags.len += write(1, format, percent - format);
 			format = percent + 1;
-			if (*format)
-				if (parse_and_print((char **)&format, args, &flags) == -1)
-					return (-1);
+			if (parse_and_print((char **)&format, args, &flags) == -1)
+				return (-1);
 		}
 		else
 		{
